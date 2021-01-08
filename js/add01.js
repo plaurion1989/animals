fbdfaf5c16729d254b098988131cac959e4e078b
plaurion1989 <plaurion@protonmail.com>
2021-01-08 12:29:19 -0800

    fixed javascript issues for loop

--- js/add01.js
+++ js/add01.js
@@ -9,5 +9,24 @@ function askUserName() {
       document.write("Welcome to the site " + animalName + " Lover!");
     } 
 }
+
+askUserName();
   
- 
+function animalQuiz() {
+  let animalQuiz = prompt('Which animal is scarier? lions, tigers or bears?');
+  
+  alert('Who likes ' + animalQuiz + ' anyway?');
+  
+  alert('If you really like lions, type lion in as your favorite animal.  If you dont, feel free to type in ANY animal you want!');
+  }
+
+animalQuiz();
+
+  function printNumbers() {
+    let userInput = prompt('How many pictures of lions do you want?')
+      document.write('<p>' + userInput + '</p>')
+    for(let i = 0; i < userInput; i++){
+      document.write('<img src="img/liontiger.jpg"></img>')
+    }
+  }
+
--- js/add02.js
+++ js/add02.js
@@ -1,7 +1,8 @@
-let animalQuiz = prompt('Which animal is scarier? lions, tigers or bears?')
+
+function animalQuiz();{
+let animalQuiz = prompt('Which animal is scarier? lions, tigers or bears?');
 
 alert('Who likes ' + animalQuiz + ' anyway?');
 
-
-
-
+alert('If you really like lions, type lion in as your favorite animal.  If you dont, feel free to type in ANY animal you want!');
+}
